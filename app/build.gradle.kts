27d5e6311e3ee536298e85f18508261d5e6d48b7
ST10050487 <103074135+ST10050487@users.noreply.github.com>
2024-09-21 17:21:53 +0200

    Added the Figureprint feture and Gooale maps feature

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,12 +1,19 @@
+import java.util.Properties // Import required for Properties class
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.google.gms.google.services)
+    alias(libs.plugins.map.secret)
 }
 
 android {
     namespace = "za.co.varsitycollage.st10050487.eventat"
     compileSdk = 34
+
+    buildFeatures {
+        buildConfig = true  // Enable BuildConfig feature
+    }
 
     defaultConfig {
         applicationId = "za.co.varsitycollage.st10050487.eventat"
@@ -16,6 +23,15 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        // Loading the MAPS_KEY from local.properties
+        val properties = Properties()
+        val localPropertiesFile = rootProject.file("local.properties")
+        if (localPropertiesFile.exists()) {
+            properties.load(localPropertiesFile.inputStream())
+            val mapsApiKey = properties["MAPS_KEY"] as String
+            buildConfigField("String", "MAPS_API_KEY", "\"$mapsApiKey\"")
+        }
     }
 
     buildTypes {
@@ -27,17 +43,18 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
     kotlinOptions {
         jvmTarget = "1.8"
     }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -45,19 +62,31 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.firebase.database)
     implementation(libs.firebase.storage)
+    implementation(libs.androidx.storage)
+    implementation(libs.play.services.tflite.support)
+    implementation(libs.play.services.maps)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     // Import the Firebase BoM
     implementation(platform("com.google.firebase:firebase-bom:33.2.0"))
-    // TODO: Add the dependencies for Firebase products you want to use
-    // When using the BoM, don't specify versions in Firebase dependencies
+    // Firebase dependencies
     implementation("com.google.firebase:firebase-analytics")
     implementation(libs.firebase.auth)
-    // Add the dependency for the Cloud Storage library
-    // When using the BoM, you don't specify versions in Firebase library dependencies
     implementation("com.google.firebase:firebase-storage")
-    // Add the dependency for the Cloud Storage library
-    // When NOT using the BoM, you must specify versions in Firebase library dependencies
     implementation("com.google.firebase:firebase-storage:21.0.0")
+    // Dependency for Bio-metric
+    implementation("androidx.biometric:biometric:1.2.0-alpha01")
+    implementation("androidx.asynclayoutinflater:asynclayoutinflater-appcompat:1.1.0-alpha01")
+    implementation("androidx.compose.ui:ui:1.0.5")
+    implementation("androidx.compose.material:material:1.0.5")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.0.5")
+    implementation("androidx.activity:activity-compose:1.3.1")
+
+    implementation(libs.play.services.maps.v1700)
+    implementation("com.google.android.gms:play-services-maps:18.0.0")
+    implementation("com.google.android.libraries.places:places:2.4.0")
+    //Implementing the Volley Dependency
+    implementation("com.android.volley:volley:1.2.1")
+
 }
