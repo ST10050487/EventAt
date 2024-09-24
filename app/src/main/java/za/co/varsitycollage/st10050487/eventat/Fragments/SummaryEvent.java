f438d928e108380e1259cefd197fc09f2d401e7a
ST100535561 <ST10053561@vcconnect.edu.za>
2024-09-25 00:14:20 +0200

    Finished the Summary Event but not the UI completely

---
+++ app/src/main/java/za/co/varsitycollage/st10050487/eventat/Fragments/SummaryEvent.java
@@ -0,0 +1,26 @@
+package za.co.varsitycollage.st10050487.eventat.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import za.co.varsitycollage.st10050487.eventat.R;
+
+
+public class SummaryEvent extends Fragment {
+
+    public SummaryEvent() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_summary_event, container, false);
+    }
+}
