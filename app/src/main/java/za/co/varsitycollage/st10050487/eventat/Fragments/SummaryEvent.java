00eed36e25d23b68fbac2bc9e8031c8fb2d57e0e
ST100535561 <ST10053561@vcconnect.edu.za>
2024-09-25 18:31:06 +0200

    Finished the Payment Method UI

--- app/src/main/java/za/co/varsitycollage/st10050487/eventat/Fragments/SummaryEvent.java
+++ app/src/main/java/za/co/varsitycollage/st10050487/eventat/Fragments/SummaryEvent.java
@@ -2,14 +2,16 @@ package za.co.varsitycollage.st10050487.eventat.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import za.co.varsitycollage.st10050487.eventat.R;
-
 
 public class SummaryEvent extends Fragment {
 
@@ -21,6 +23,46 @@ public class SummaryEvent extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_summary_event, container, false);
+        View view = inflater.inflate(R.layout.fragment_summary_event, container, false);
+
+        // Find the arrowButton and set an OnClickListener
+        view = MovingToStageMethod(view);
+
+        return MovingToPayment(view);
+    }
+
+    private @NonNull View MovingToPayment(View view) {
+        // Find the next button and set an OnClickListener
+        Button nextButton = view.findViewById(R.id.EventSubmitButton);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to PaymentMethod fragment
+                Fragment paymentMethodFragment = new PaymentMethod();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_EventInfo_container, paymentMethodFragment);
+                transaction.addToBackStack(null); // Add to back stack to allow back navigation
+                transaction.commit();
+            }
+        });
+
+        return view;
+    }
+
+    private @NonNull View MovingToStageMethod(View view) {
+        Button arrowButton = view.findViewById(R.id.arrowButton);
+        arrowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to ChoosingTicketStage fragment
+                Fragment choosingTicketStageFragment = new ChoosingTicketStage();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_EventInfo_container, choosingTicketStageFragment);
+                transaction.addToBackStack(null); // Add to back stack to allow back navigation
+                transaction.commit();
+            }
+        });
+
+        return view;
     }
 }
