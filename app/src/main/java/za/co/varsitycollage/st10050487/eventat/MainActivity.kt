7d82cca1e5874ba8de0e586138cae8a22eba1427
ST100535561 <ST10053561@vcconnect.edu.za>
2024-09-30 23:14:38 +0200

    Update MainActivity.kt

--- app/src/main/java/za/co/varsitycollage/st10050487/eventat/MainActivity.kt
+++ app/src/main/java/za/co/varsitycollage/st10050487/eventat/MainActivity.kt
@@ -1,30 +1,15 @@
 package za.co.varsitycollage.st10050487.eventat
 
-import android.Manifest
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import za.co.varsitycollage.st10050487.eventat.Fragments.HomeScreen
 
 class MainActivity : AppCompatActivity() {
-
-    private val requestPermissionLauncher = registerForActivityResult(
-        ActivityResultContracts.RequestPermission()
-    ) { isGranted: Boolean ->
-        if (isGranted) {
-            // Permission is granted. Continue with posting notifications.
-        } else {
-            // Permission is denied. Handle the case.
-        }
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -36,27 +21,6 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        // Request POST_NOTIFICATIONS permission if needed
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            when {
-                ContextCompat.checkSelfPermission(
-                    this,
-                    Manifest.permission.POST_NOTIFICATIONS
-                ) == PackageManager.PERMISSION_GRANTED -> {
-                    // Permission is already granted. Continue with posting notifications.
-                }
-
-                shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS) -> {
-                    // Show an explanation to the user why the permission is needed.
-                }
-
-                else -> {
-                    // Directly request the permission.
-                    requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
-                }
-            }
-        }
-
         // Delay of 3 seconds (3000 milliseconds)
         Handler(Looper.getMainLooper()).postDelayed({
             // Navigate to GoogleMapsAPI Activity after the delay
@@ -64,7 +28,7 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
             // Finishing MainActivity so that the user cannot go back to it
             finish()
-            // Moving to the next screen after 3 seconds
+            //Moving to the next screen after 3 seconds
         }, 3000)
     }
 }
