97422f638ad4ed2089a5c3ad34cf2957e3ca2d1a
ST100535561 <ST10053561@vcconnect.edu.za>
2024-09-30 10:48:33 +0200

    update ticketDownload.java

--- app/src/main/java/za/co/varsitycollage/st10050487/eventat/Fragments/ticketDownload.java
+++ app/src/main/java/za/co/varsitycollage/st10050487/eventat/Fragments/ticketDownload.java
@@ -4,9 +4,6 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.lifecycle.ViewModelProvider;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
